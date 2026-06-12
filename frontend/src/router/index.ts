e339656823c2371c9c3327eff848d4a1a6792a6a
liuyueco <1831264870@qq.coom>
2026-06-12 10:13:18 +0800

    fix(router): update Home component import path to use new directory structure

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -33,7 +33,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/home',
     name: 'Home',
-    component: () => import('@/views/HomeView.vue'),
+    component: () => import('@/views/home/index.vue'),
     meta: {
       requiresAuth: false,
       title: 'Home'
